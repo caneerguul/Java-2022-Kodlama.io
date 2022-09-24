611b8b1fd69959a00ee79a5fa2f1cb273e4e643b
caneerguul <96837237+caneerguul@users.noreply.github.com>
2022-09-24 15:11:36 +0300

    Add files via upload

---
+++ methodOverloading/Main.java
@@ -0,0 +1,9 @@
+package ödev2;
+
+public class Main {
+	public static void main(String[] args) {
+		DortIslem dortIslem = new DortIslem();
+		System.out.println(dortIslem.topla(6, 9));
+		System.out.println(dortIslem.topla(5, 12, 23));
+	}
+}
