ae33836d9c2e5f266b133ea35043f660e2039e63
caneerguul <96837237+caneerguul@users.noreply.github.com>
2022-09-24 15:09:58 +0300

    Add files via upload

---
+++ classes/Main.java
@@ -0,0 +1,24 @@
+package ödev2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		CustomerManager customerManager = new CustomerManager();
+		customerManager.Add();
+		customerManager.Remove();
+		customerManager.Update();
+		
+		int sayi1 = 10;
+		int sayi2 = 20;
+
+		sayi2 = sayi1;
+		sayi1 = 30;
+		System.out.println(sayi2);
+		
+		int[] sayilar1 = new int[] {1,2,3};
+		int[] sayilar2 = {4,5,6};
+		sayilar2=sayilar1;
+		sayilar1[0]=10;
+		System.out.println(sayilar2[0]);
+	}
+}
